0117fe3fa7b8a179dde72249d6782ee3cfb22997
sergiodserbluk <sergiodserbluk@gmail.com>
2025-07-18 09:28:44 -0300

    se incluye la configuracion de los path

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+// __DIR__ en este contexto es /minidespensa/config
+define('BASE_PATH', realpath(__DIR__ . '/..')); // sube a /minidespensa
+
+define('INCLUDES_PATH', BASE_PATH . '/includes');
+define('PUBLIC_PATH', BASE_PATH . '/public');
+define('MODULES_PATH', BASE_PATH . '/modules');
+define('AUTH_PATH', BASE_PATH . '/auth');
+
--- includes/menu.php
+++ includes/menu.php
@@ -8,19 +8,19 @@
 
         <?php if (in_array($_SESSION['rol'], ['admin', 'compras'])): ?>
             <li class="nav-item">
-                <a class="nav-link" href="/modules/compras/">Compras</a>
+                <a class="nav-link" href="../modules/compras/">Compras</a>
             </li>
         <?php endif; ?>
 
         <?php if (in_array($_SESSION['rol'], ['admin', 'vendedor'])): ?>
             <li class="nav-item">
-                <a class="nav-link" href="/modules/ventas/">Facturación</a>
+                <a class="nav-link" href="../modules/ventas/">Facturación</a>
             </li>
         <?php endif; ?>
 
         <?php if (in_array($_SESSION['rol'], ['admin'])): ?>
             <li class="nav-item">
-                <a class="nav-link" href="/modules/usuarios/">Usuarios</a>
+                <a class="nav-link" href="../modules/usuarios/index.php">Usuarios</a>
             </li>
         <?php endif; ?>
 
